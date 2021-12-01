5273277798fa88527656203313b29f4c7dfe7629
Maik Ruckauf <maik@ad.unc.edu>
2021-12-01 18:32:56 -0500

    fixed a typo

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -31,7 +31,7 @@ export function getMedian(array) {
     });
     var half = Math.floor(array.length/2);
     if (array.length%2) {return array[half]}
-    return (array[half-1]+array[hald])/2.0;
+    return (array[half-1]+array[half])/2.0;
 }
 
 /**
