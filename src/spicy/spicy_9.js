997f47eb4000980ce3ecb382da47c781b58d109f
Maik Ruckauf <maik@ad.unc.edu>
2021-12-02 12:47:36 -0500

    trying some more functions

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -23,7 +23,7 @@ export const repeat = (fn, n, ...params) => {
  * Use the repeat function to log the string "Hello, world!" to the console
  *   10 times.
  */
-export const repeatDemo = () => {return repeat(console.log, 10, "Hello world!");};
+export const repeatDemo = () => {return repeat(console.log, 10, "Hello, world!");};
 
 
 /**************************************************************************
@@ -79,7 +79,10 @@ export const tenTimesFifty = () => {return tenTimes(50);};
  *    everyEven([1, 1, 0, 1, 1], x => x === 1)  <--  returns false
  */
 export const everyEven = (arr, test) => {
-
+    for (let i = 0; i < arr.length; i += 2) {
+        if (!test(arr[i])) {return false;}
+    }
+    return true;
 };
 
 
@@ -103,7 +106,10 @@ export const everyEven = (arr, test) => {
  *    someEven([0, 0, 0, 0, 0], x => x === 0)  <--  returns true
  */
 export const someEven = (arr, test) => {
-
+    for (let i = 0; i < arr.length; i += 2) {
+        if (test(arr[i])) {return true;}
+    }
+    return false;
 };
 
 
@@ -139,7 +145,7 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-
+    return everyEven(arr, num % 2);
 };
 
 
@@ -149,7 +155,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-
+    return someEven(arr, num % 2);
 };
 
 
@@ -160,5 +166,5 @@ export const anEvenIsOdd = (arr) => {
  *   pass the test. You must use the filter function.
  */
 export const hasExactly = (arr, test, n) => {
-
+    
 };
