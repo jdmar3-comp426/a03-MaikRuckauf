728ef9c85d3fc86c41783b9a474f8ef093baf54c
Maik Ruckauf <maik@ad.unc.edu>
2021-12-01 20:52:43 -0500

    typo

--- src/medium/medium_3.js
+++ src/medium/medium_3.js
@@ -18,8 +18,9 @@ queries.
  *
  */
 export function searchHighPower(car_data, minHorsepower, minTorque) {
-    let arr = car_data.filter((object) => object.horsepower >= minHorsepower && object.torque >= minTorque)
-    return arr.sort((a,b) => (a.horsepower < b.horsepower) ? 1 : -1).reverse();
+    let arr = car_data.filter((object) => object.horsepower >= minHorsepower && object.torque >= minTorque);
+    return arr;
+    //return arr.sort((a,b) => (a.horsepower < b.horsepower) ? 1 : -1).reverse();
 }
 
 
